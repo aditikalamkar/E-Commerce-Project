147c5b06ec16445d9aafe261adfc578eda070928
aditikalamkar <aditikalamkar20@gmail.com>
2024-04-10 12:00:26 +0530

    First Commit

---
+++ 02-Backend/E-commerce-Portal/src/main/java/com/project/EcommercePortal/ECommercePortalApplication.java
@@ -0,0 +1,13 @@
+package com.project.EcommercePortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePortalApplication.class, args);
+	}
+
+}
